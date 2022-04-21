d29069bfeb21f721a6b9df5c70f3db5b597d9db9
Shane Perry <perrysm98@gmail.com>
2022-04-21 18:56:21 -0500

    Added functionality to review locations- Review.java/ReviewController.java

--- src/application/controller/ReviewController.java
+++ src/application/controller/ReviewController.java
@@ -1,14 +1,24 @@
 package application.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import application.Home;
 import application.model.Faq;
 import application.model.Recommended;
+import application.model.Review;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 
-public class ReviewController {
-		
+
+public class ReviewController implements Initializable{
+	
+		String review;
+		String selectedLocation;
 		Home home;
 		Faq faq;
 		Recommended recommended;
@@ -16,6 +26,18 @@ public class ReviewController {
 			recommended = new Recommended();
 			home = new Home();
 			faq = new Faq();
+		}
+		
+		@FXML
+	    private ComboBox<String> placeList;
+
+	    @FXML
+	    private TextArea reviewText;
+	    
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			
+			Review.loadData(placeList);
 		}
 		
 	    @FXML
@@ -35,9 +57,14 @@ public class ReviewController {
 	    	Scene scene = faq.getScene();
 	    	Home.stage.setScene(scene);
 	    }
-
+	    
 	    @FXML
-	    void processFAQ(ActionEvent event) {
-
-	    }
+	    void clickSubmit(ActionEvent event) {
+	    	review = reviewText.getText();
+	    	selectedLocation = placeList.getSelectionModel().getSelectedItem();
+	    	
+	    	Review.createReview(review, selectedLocation);
+	    	reviewText.clear();
+	    	placeList.valueProperty().set(null);
+	    } 
 	}
--- src/application/model/Review.java
+++ src/application/model/Review.java
@@ -1,12 +1,18 @@
 package application.model;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 
 public class Review {
+		
 	public Scene scene;
 	public Scene getScene() {
 		if (scene == null) { //checking if scene null before creating scene again
@@ -31,5 +37,47 @@ public class Review {
 		}
 		return scene;
 	}
+	public static void loadData(ComboBox<String> placeList) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("./other/locations.txt"));
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			
+			while(line != null) {
+				placeList.getItems().add(line);
+				
+				sb.append(line);
+				line = br.readLine();
+			}
+			br.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public static void createReview(String review, String location) {
+		
+		File file = new File("./other/reviews.txt");
+		BufferedWriter bf = null;
+		
+		try {
+			bf = new BufferedWriter(new FileWriter(file, true));	
+				bf.write(location + "," + review);
+				bf.newLine();
+				
+		}catch(IOException e) {
+			e.printStackTrace();		
+			}
+		finally {
+			try {
+				bf.flush();
+				bf.close();
+			}
+			catch (Exception e) {
+			}
+		}
+	}
+	
+	
 
 }
