e00c63d7c301c77d2b3901690f02681ad5c5617a
David Lambert <david.lambert@my.utsa.edu>
2022-04-22 12:36:42 -0500

    added comments to HomeController

--- src/application/controller/HomeController.java
+++ src/application/controller/HomeController.java
@@ -3,7 +3,6 @@ package application.controller;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
 import application.Home;
 import application.model.Faq;
 import application.model.Recommended;
@@ -13,21 +12,30 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 
+/**
+ * The HomeController handles the flow of data between Home.java and 
+ * the home.fxml view file. It loads the locations from the two files
+ * and displays it into the appropriate fxml text area
+ */
 public class HomeController {
+	//class variables
 	Faq faq;
 	Review review;
 	Recommended recommended;
 	
+	//private fxml elements of the view
 	@FXML
     private TextArea recommend, recent;
 	
+	//generator, called when homecontroller class is called from another file
 	public HomeController() {
 		faq = new Faq();
 		review = new Review();
 		recommended = new Recommended();
 	}
 	
-	
+	//init method, called when object is first created to set up class state
+	//Reads in the two files line by line and appends to appropriate textarea
 	 public void initialize() {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("./other/suggested.txt"));
@@ -56,26 +64,21 @@ public class HomeController {
 			e.printStackTrace();		
 		}
     }
-	
-	
-	
+	//These last methods are to navigate to the relevant page when clicked on in the fxml file
 	@FXML
     void navToFaqs(ActionEvent event) {
 		Scene scene = faq.getScene();
     	Home.stage.setScene(scene);
     }
-	
 	@FXML
     void navToRecommended(ActionEvent event) {
 			Scene scene = recommended.getScene();
 	    	Home.stage.setScene(scene);
 	    }
-		
 	@FXML
     void navToReview(ActionEvent event) {
 		Scene scene = review.getScene();
     	Home.stage.setScene(scene);
     
     }
-
 }
