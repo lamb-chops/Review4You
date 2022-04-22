08fde53efc19300eec31632c0f23c7ec941074cc
David Lambert <david.lambert@my.utsa.edu>
2022-04-22 12:17:30 -0500

    set up locations to be read in home page at startup

--- src/application/controller/HomeController.java
+++ src/application/controller/HomeController.java
@@ -1,5 +1,9 @@
 package application.controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import application.Home;
 import application.model.Faq;
 import application.model.Recommended;
@@ -7,17 +11,53 @@ import application.model.Review;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 
 public class HomeController {
 	Faq faq;
 	Review review;
 	Recommended recommended;
 	
+	@FXML
+    private TextArea recommend, recent;
+	
 	public HomeController() {
 		faq = new Faq();
 		review = new Review();
 		recommended = new Recommended();
 	}
+	
+	
+	 public void initialize() {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("./other/suggested.txt"));
+			String line = br.readLine();
+			
+			while(line != null) {
+				recommend.appendText(line+"\n");
+				line = br.readLine();
+			}			
+			br.close();
+		} catch(IOException e) {
+			e.printStackTrace();		
+		}
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("./other/locations.txt"));
+			String line = br.readLine();
+			
+			while(line != null) {
+				recent.appendText(line+"\n");
+				line = br.readLine();
+			}
+			
+			br.close();
+		} catch(IOException e) {
+			e.printStackTrace();		
+		}
+    }
+	
+	
 	
 	@FXML
     void navToFaqs(ActionEvent event) {
